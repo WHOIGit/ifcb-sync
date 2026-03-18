69932b3bdf29e9ed613ea111c4d4145c0a22a943
ethanandrews <eandrews@whoi.edu>
2026-03-18 09:11:12 -0400

    add arm specific compiled build

--- aws-ifcb-data-sharer/ifcb-file-watcher/go-build.sh
+++ aws-ifcb-data-sharer/ifcb-file-watcher/go-build.sh
@@ -7,3 +7,6 @@ GOOS=windows GOARCH=amd64 go build -o ifcb-file-watcher-windows
 
 # Mac compile
 GOOS=darwin GOARCH=arm64 go build -o ifcb-file-watcher-macos
+
+# Linux ARM (Jetson, Raspberry PI, etc) compile
+GOOS=linux GOARCH=arm go build -o ifcb-file-watcher-arm
